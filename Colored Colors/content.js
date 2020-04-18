aed93fbdae3ef736d6f4ccac923b950967fd5baf
James <jamestedesco802@gmail.com>
2020-04-17 22:50:35 -0400

    Working baseline commit
    Basic colors render in the lowercase, proper case, and plural forms. Also recognizes RGB & CYMK

--- Colored Colors/content.js
+++ Colored Colors/content.js
@@ -1,4 +1,24 @@
+//TODOL add recognition for: CMYK, RGB
+
+main()
+
 function main() {
+
+    // get main text by paragraph tag
+    var allParagraphs = document.getElementsByTagName("p")
+
+    for (para of allParagraphs) {
+        // check the document for that color
+        checkPageForColor(para)
+    }
+
+    console.log("That's all folks!")
+}
+
+
+function checkPageForColor(para) {
+
+
     const cssColors = [
         "AliceBlue",
         "AntiqueWhite",
@@ -150,45 +170,143 @@ function main() {
         "YellowGreen",
     ];
 
-    // for every valid color in CSS
+    const pluralizedColors = ["AliceBlues", "AntiqueWhites", "Aquas", "Aquamarines", "Azures", "Beiges", "Bisques", "Blacks", "BlanchedAlmonds", "Blues", "BlueViolets", "Browns", "BurlyWoods", "CadetBlues", "Chartreuses", "Chocolates", "Corals", "CornflowerBlues", "Cornsilks", "Crimsons", "Cyans", "DarkBlues", "DarkCyans", "DarkGoldenRods", "DarkGrays", "DarkGreys", "DarkGreens", "DarkKhakis", "DarkMagentas", "DarkOliveGreens", "DarkOranges", "DarkOrchids", "DarkReds", "DarkSalmons", "DarkSeaGreens", "DarkSlateBlues", "DarkSlateGrays", "DarkSlateGreys", "DarkTurquoises", "DarkViolets", "DeepPinks", "DeepSkyBlues", "DimGrays", "DimGreys", "DodgerBlues", "FireBricks", "FloralWhites", "ForestGreens", "Fuchsias", "Gainsboros", "GhostWhites", "Golds", "GoldenRods", "Grays", "Greys", "Greens", "GreenYellows", "HoneyDews", "HotPinks", "IndianReds", "Indigos", "Ivorys", "Khakis", "Lavenders", "LavenderBlushs", "LawnGreens", "LemonChiffons", "LightBlues", "LightCorals", "LightCyans", "LightGoldenRodYellows", "LightGrays", "LightGreys", "LightGreens", "LightPinks", "LightSalmons", "LightSeaGreens", "LightSkyBlues", "LightSlateGrays", "LightSlateGreys", "LightSteelBlues", "LightYellows", "Limes", "LimeGreens", "Linens", "Magentas", "Maroons", "MediumAquaMarines", "MediumBlues", "MediumOrchids", "MediumPurples", "MediumSeaGreens", "MediumSlateBlues", "MediumSpringGreens", "MediumTurquoises", "MediumVioletReds", "MidnightBlues", "MintCreams", "MistyRoses", "Moccasins", "NavajoWhites", "Navys", "OldLaces", "Olives", "OliveDrabs", "Oranges", "OrangeReds", "Orchids", "PaleGoldenRods", "PaleGreens", "PaleTurquoises", "PaleVioletReds", "PapayaWhips", "PeachPuffs", "Perus", "Pinks", "Plums", "PowderBlues", "Purples", "RebeccaPurples", "Reds", "RosyBrowns", "RoyalBlues", "SaddleBrowns", "Salmons", "SandyBrowns", "SeaGreens", "SeaShells", "Siennas", "Silvers", "SkyBlues", "SlateBlues", "SlateGrays", "SlateGreys", "Snows", "SpringGreens", "SteelBlues", "Tans", "Teals", "Thistles", "Tomatos", "Turquoises", "Violets", "Wheats", "Whites", "WhiteSmokes", "Yellows", "YellowGreens"];
+
+    // these colors will be specially given black backgrounds; use sparingly
+    const lightColors = ["white", "whites", "azure", "azures"]
+
+
+    // check all colors
     for (color of cssColors) {
-        // check the document for that color
-        checkPageForColor(color)
+        checkLowerCase(color, para)
+        checkCapitalized(color, para)
+
     }
 
+    for (color of pluralizedColors) {
+        checkPlural(color, para)
+    }
+
+
+    checkEdgeCases(para)
 
 
 }
 
 
-function checkPageForColor(color) {
-
+function checkLowerCase(color, para) {
     // lowercase for majority of reading
-    color = color.toLowerCase()
-        // trying to decrease number of words that include colors
-    var spacedColor = ` ${color}`
+    var color = color.toLowerCase()
 
-    if (color == "yellow") {
-        color = 'gold'
-    }
+    // contains at least one instance of the color's word
+    var paraString = para.innerText
+
+    // if the desired color exists, edit the element
+    if (paraString.includes(color)) {
+        console.log(color, "found!")
 
 
-    // get main text by paragraph tag
-    var allParagraphs = document.getElementsByTagName("p")
+        // "\\" escapes the first backslash, allowing it; "\b" == word boundary;
+        // "g" == global search; "i" == case-insensitive;
+        re = new RegExp("\\b" + color + "\\b", "gi");
 
-    for (para of allParagraphs) {
-        // if the desired color exists, edit the element
-        if (para.innerText.includes(spacedColor)) {
-            console.log(para)
+        // general insert for color change
+        var styleParams = `<span style="color:${color};font-weight:bolder;">${color}</span>`
 
-            // this element is contains at least one instance of the word
-            var paraString = para.innerText
-
-            var revisedParaString = paraString.replace(spacedColor, `<span style="color:${color};">${spacedColor}</span>`);
-
-            para.innerHTML = revisedParaString
+        // add background-color if text color is too light
+        if (color == "white") {
+            styleParams = `<span style="color:${color};font-weight:bolder;background-color:black;padding:0 3px;border-radius:3px;">${color}</span>`
+        } else if (color == "yellow") {
+            styleParams = `<span style="color:gold;font-weight:bolder;">${color}</span>`
         }
+
+
+        // add color to website HTML
+        para.innerHTML = para.innerHTML.replace(re, styleParams)
     }
 }
 
-main()
+function checkCapitalized(color, para) {
+
+    // contains at least one instance of the color's word
+    var paraString = para.innerText
+
+    // if the desired color exists, edit the element
+    if (paraString.includes(color)) {
+        console.log(color, "found!")
+
+
+        // "\\" escapes the first backslash, allowing it; "\b" == word boundary;
+        // "g" == global search; "i" == case-insensitive;
+        re = new RegExp("\\b" + color + "\\b", "gi");
+
+
+        var styleParams = `<span style="color:${color};font-weight:bolder;">${color}</span>`
+        if (color == "white") {
+            styleParams = `<span style="color:${color};font-weight:bolder;background-color:black;padding:0 3px;border-radius:3px;">${color}</span>`
+        } else if (color == "yellow") {
+            styleParams = `<span style="color:gold;font-weight:bolder;">${color}</span>`
+        }
+
+
+        // add color to website HTML
+        para.innerHTML = para.innerHTML.replace(re, styleParams)
+    }
+}
+
+function checkPlural(color, para) {
+    // lowercase for majority of reading
+    var color = color.toLowerCase()
+
+    // contains at least one instance of the color's word
+    var paraString = para.innerText
+
+    // if the desired color exists, edit the element
+    if (paraString.includes(color)) {
+        console.log(color, "found!")
+
+
+        // "\\" escapes the first backslash, allowing it; "\b" == word boundary;
+        // "g" == global search; "i" == case-insensitive;
+        re = new RegExp("\\b" + color + "\\b", "gi");
+
+
+        var styleParams = `<span style="color:${color.slice(0,-1)};font-weight:bolder;">${color}</span>`
+        if (color == "whites") {
+            styleParams = `<span style="color:${color.slice(0,-1)};font-weight:bolder;background-color:black;padding:0 3px;border-radius:3px;">${color}</span>`
+        } else if (color == "yellows") {
+            styleParams = `<span style="color:gold;font-weight:bolder;">${color}</span>`
+        }
+
+
+        // add color to website HTML
+        para.innerHTML = para.innerHTML.replace(re, styleParams)
+    }
+}
+
+function checkEdgeCases(para) {
+
+    // contains at least one instance of the color's word
+    var paraString = para.innerText
+
+    // check cyan, magenta, yellow, and key/black
+    if (paraString.includes('CMYK')) {
+        re = new RegExp("\\b" + 'CMYK' + "\\b", "gi");
+
+        // wrap coloring here
+        stylizedSpan = `<span style="color:cyan;font-weight:bolder;">C</span><span style="color:magenta;font-weight:bolder;">M</span><span style="color:gold;font-weight:bolder;">Y</span><span style="color:black;font-weight:bolder;">K</span>`
+
+        // commit changes to website
+        para.innerHTML = para.innerHTML.replace(re, stylizedSpan)
+
+        // check for RBG string
+    } else if (paraString.includes('RGB')) {
+        re = new RegExp("\\b" + 'RGB' + "\\b", "gi");
+
+        // wrap coloring here
+        stylizedSpan = `<span style="color:red;font-weight:bolder;">R</span><span style="color:green;font-weight:bolder;">G</span><span style="color:blue;font-weight:bolder;">B</span>`
+
+        // commit changes to website
+        para.innerHTML = para.innerHTML.replace(re, stylizedSpan)
+    }
+}
