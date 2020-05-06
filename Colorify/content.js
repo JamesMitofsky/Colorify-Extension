aa403dec0d85e7516c451c85b0a5145162b99cc7
James <jamestedesco802@gmail.com>
2020-05-06 14:30:11 -0400

    Revert "Added all light colors in plural form & minified published extension"
    This reverts commit dfb5ad7d89b88736eaf4f56f3c43a6f7d6f82d8e.

--- Colorify/content.js
+++ Colorify/content.js
@@ -253,7 +253,7 @@ function checkSpecificInstances(sentColor, para) {
 // determines if dark background is needed
 function isColorLight(cssColor) {
     // these colors will be specially given black backgrounds; use sparingly
-    const lightColors = ["white","whites", "azure","azures", "ivory", "ivories", "linen", "linens", "lavender", "lavenders", "moccasin", "moccasins", "snow", "snows"]
+    const lightColors = ["white", "azure", "ivory", "linen", "lavender", "moccasin", "snow"]
 
     // test if the current color is light
     let activeLightColor = lightColors.some(testingColor => {
